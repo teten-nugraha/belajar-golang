6798979067b87d15151584b2332c334ad3603728
teten777 <teten.nugraha18@gmail.com>
2018-09-20 11:22:58 +0700

    web api

---
+++ 24. web api json/1test.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type student struct {
+	ID    string
+	Name  string
+	Grade int
+}
+
+var data = []student{
+	student{"ESSD", "Teten Nugraha", 90},
+	student{"DFSDF", "Yasna", 90},
+	student{"4DF", "Abbu", 90},
+	student{"6DDV", "Appa", 90},
+}
+
+func users(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == "POST" {
+		var result, err = json.Marshal(data)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(result)
+		return
+
+	} else if r.Method == "GET" {
+		data := "GET Ieu mah"
+		var result, err = json.Marshal(data)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(result)
+		return
+	}
+
+	http.Error(w, "", http.StatusBadGateway)
+}
+
+func user(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == "POST" {
+		var id = r.FormValue("id")
+		var result []byte
+		var err error
+
+		for _, each := range data {
+			if each.ID == id {
+				result, err = json.Marshal(each)
+
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+
+				w.Write(result)
+				return
+			}
+		}
+
+		http.Error(w, "User not found", http.StatusBadRequest)
+		return
+	}
+
+	http.Error(w, "", http.StatusBadRequest)
+
+}
+
+func main() {
+	http.HandleFunc("/users", users)
+	http.HandleFunc("/user", user)
+
+	fmt.Println("starting web server at http://localhost:8000")
+	http.ListenAndServe(":8000", nil)
+}
